3a17ed934d8e54cc620ee4b2875ed0ebc979b298
glitched0xff <mauro.prandelli@gmail.com>
2026-02-24 16:55:34 +0100

    Update

--- mqtt/handlerZig2Mqtt.js
+++ mqtt/handlerZig2Mqtt.js
@@ -9,6 +9,7 @@ let debounchTime=5000
 function parseZigBeeTHSonoff(topic,inputJson) {
    inputJson=JSON.parse(inputJson)
    //console.log(inputJson)
+   
   const fieldMap = {
     temperature: 'temp',
     humidity: 'hume',
